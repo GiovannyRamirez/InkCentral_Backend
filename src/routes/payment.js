a7c13ca1326b754530c5dc07de797d84e008b3f0
Ivan Gonzalez <gonzalez.ivand@gmail.com>
2020-12-28 12:15:15 -0500

    Remove commented section of code

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -3,6 +3,6 @@ const paymentController = require('../controllers/payment.controller')
 const  { auth } = require('../utils/auth')
 
 router.route('/:artistId').post(auth, paymentController.create);
-// router.route('/:paymentId').get(paymentController.show)
+
 
 module.exports = router;
