8aa89b565b4bcfd018f34e54626f6f9ee3b40d01
GiovannyRamirez <giovannyramirezs@hotmail.com>
2021-01-17 21:18:46 -0500

    Delete model, controller and route for appointments, this data will be managed from payments

--- src/routes/appointment.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const appointmentController = require('../controllers/appointment.controller')
-const { auth } = require('../utils/auth')
-
-router.route('/:artistId').post(auth, appointmentController.create)
-
-module.exports = router
